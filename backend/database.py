fdf0a9390d5e280c79128efa6924e0843d00165c
Sunrise443 <sunkensun0@gmail.com>
2024-06-28 23:28:43 +0300

    Adding database and fixing parsing

---
+++ backend/database.py
@@ -0,0 +1,20 @@
+from venv import create
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+engine = create_async_engine("sqlite+aiosqlite:///backend/parsed_data.db")
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+class Model(DeclarativeBase):
+    pass
+
+class VacancyOrm(Model):
+    __tablename__ = "vacancies"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    vacancy_name: Mapped[str]
+    vacancy_salary: Mapped[str]
+    vacancy_expiriens: Mapped[str]
+    vacancy_place: Mapped[str]
+    vacancy_link
--- backend/parsing.py
+++ backend/parsing.py
@@ -1,12 +1,14 @@
-from turtle import heading
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-name = str(input("What you're searching for: "))
-url = f"https://hh.ru/search/vacancy?text={name}"
+vac_name = str(input("Введите название вакансии: "))
+vac_salary = int(input("Введите желаемую зарплату: "))
+vac_experience = "between1And3"
+
+url = f"https://hh.ru/search/vacancy?text={vac_name}&salary={vac_salary}&experience={vac_experience}&items_on_page=5"
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
@@ -14,11 +16,16 @@ driver.get(url)
 
 soup = BeautifulSoup(driver.page_source, features='lxml')
 
-headings = soup.find_all(name='span', attrs={'class': "vacancy-name--c1Lay3KouCl7XasYakLk serp-item__title-link"})
+vacancies = soup.find_all(name='div', attrs={'class': "vacancy-card--z_UXteNo7bRGzxWVcL7y font-inter"})
 
-for heading in headings:
-    print(heading.getText())
+for vacancy in vacancies:
+    vacancy_name = vacancy.find(name='span', attrs={'class':'vacancy-name--c1Lay3KouCl7XasYakLk serp-item__title-link'}).getText()
+    vacancy_salary = vacancy.find(name='span', attrs={'class':'fake-magritte-primary-text--Hdw8FvkOzzOcoR4xXWni compensation-text--kTJ0_rp54B2vNeZ3CTt2 separate-line-on-xs--mtby5gO4J0ixtqzW38wh'})
+    vacancy_experience = vacancy.find(name='span', attrs={'class':'label--rWRLMsbliNlu_OMkM_D3 label_light-gray--naceJW1Byb6XTGCkZtUM'}).getText()
+    vacancy_place = vacancy.find(name='span', attrs={'data-qa':"vacancy-serp__vacancy-address", 'class':'bloko-text'}).getText()
+    vacancy_link = vacancy.find(name='a', attrs={'class':'bloko-link'}).get('href')
+    print(vacancy_name)
+
 
 time.sleep(10)
-
 driver.quit()
